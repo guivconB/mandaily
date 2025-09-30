6220c71b400759e983a5d718a978299556816c0b
Leonardo Silva <leonardosergipe03@gmail.com>
2025-09-30 11:46:52 -0300

    Construção inicial de rotas com banco de dados

---
+++ database/dbConnection.js
@@ -0,0 +1,22 @@
+// dbConnection.js
+import mongoose from "mongoose";
+
+async function conectaNaDatabase() {
+  try {
+    // Conecta usando a variável de ambiente MONGODB_URI (ainda preciso ter arquivo .env na raiz do projeto)
+    await mongoose.connect(process.env.MONGODB_URI,//colocar a string de conexão do MongoDB aqui
+      {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("Conexão com o banco de dados realizada com sucesso!");
+  } catch (error) {
+    console.error("Erro ao conectar ao banco de dados:", error);
+    process.exit(1); // Encerra a aplicação em caso de falha
+  }
+
+  return mongoose.connection;
+}
+
+export default conectaNaDatabase;

c9290aca2a6e14d70e1dfabcf72ce606167faaff
guivconB <guivcon@gmail.com>
2025-10-08 17:26:21 -0300

    Tela 4 50% pronta

---
+++ lib/passos/passo4.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class Passo4 extends StatelessWidget {
+  const Passo4({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF1E1E1E),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'lib/assets/images/iconepessoa.png', // imagem diferente se quiser
+              width: 400,
+              height: 150,
+            ),
+            const SizedBox(height: 20),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                "OOOOOOO.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            IconButton(
+              icon: const Icon(Icons.arrow_forward_ios, color: Colors.white, size: 40),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Passo4()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

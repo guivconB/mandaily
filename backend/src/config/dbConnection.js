02b32937c39b36aff711184f5872c46b4c223a0d
Leonardo Silva <leonardosergipe03@gmail.com>
2025-10-13 22:09:22 -0300

    Adicionando .env e ajustes

--- backend/src/config/dbConnection.js
+++ backend/src/config/dbConnection.js
@@ -1,20 +1,19 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); // <-- Carrega as variáveis do .env
 
 async function conectaNaDatabase() {
   try {
-    // Conecta usando a variável de ambiente MONGODB_URI (ainda precisa colocar no arquivo .env na raiz do projeto)
-    await mongoose.connect(process.env.MONGODB_URI,//colocar a string de conexão do MongoDB aqui
-      {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-
     console.log("Conexão com o banco de dados realizada com sucesso!");
   } catch (error) {
     console.error("Erro ao conectar ao banco de dados:", error);
-    process.exit(1); // Encerra a aplicação em caso de falha
+    process.exit(1);
   }
-
   return mongoose.connection;
 }
 

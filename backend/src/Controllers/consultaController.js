8cfc0566a5123241ba67f8474c9d31be5bdb182e
Victor Johann <victorjohann1404@gmail.com>
2025-10-24 17:00:25 -0300

    Atualização de Consulta Controller

--- backend/src/Controllers/consultaController.js
+++ backend/src/Controllers/consultaController.js
@@ -3,11 +3,26 @@ import Consulta from '../models/consultaModel.js';
 // Criar nova consulta
 export const criarConsulta = async (req, res) => {
 	try {
-		const consulta = new Consulta(req.body);
-		await consulta.save();
-		res.status(201).json(consulta);
+		const { userId, nomeConsulta, nomeProfissional, endereco, horario, data } = req.body;
+		
+		if(!userId || !data) {
+			return res.status(400).json({ error: "Campos Obrigatórios" });
+		}
+
+		const novaConsulta = new Consulta({
+			userId,
+			nomeConsulta,
+			nomeProfissional,
+			endereco,
+			horario,
+			data
+		});
+
+		await novaConsulta.save();
+		res.status({ message: "Consulta criada com sucesso" });
+
 	} catch (error) {
-		res.status(400).json({ error: error.message });
+		res.status(400).json({ error: "Erro ao criar Consulta" });
 	}
 };
 
@@ -17,7 +32,7 @@ export const listarConsultas = async (req, res) => {
 		const consultas = await Consulta.find();
 		res.json(consultas);
 	} catch (error) {
-		res.status(500).json({ error: error.message });
+		res.status(500).json({ error: "Erro ao listar consultas" });
 	}
 };
 
@@ -48,8 +63,8 @@ export const deletarConsulta = async (req, res) => {
 	try {
 		const consulta = await Consulta.findByIdAndDelete(req.params.id);
 		if (!consulta) return res.status(404).json({ error: 'Consulta não encontrada' });
-		res.json({ message: 'Consulta deletada com sucesso' });
+		res.status(200).json({ message: 'Consulta deletada com sucesso' });
 	} catch (error) {
-		res.status(500).json({ error: error.message });
+		res.status(400).json({ error: "Falha ao deletar Consulta" });
 	}
 };

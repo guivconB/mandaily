7a236d69ed7093e64b88995306a234a525f80afc
guivconB <guivcon@gmail.com>
2025-11-04 22:50:41 -0300

    Correcoes de erros

--- lib/perfil/perfil.dart
+++ lib/perfil/perfil.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:io'; //Adicionado para usar File
+import 'package:image_picker/image_picker.dart'; //Adicionado para selecionar imagem
 import '../widgets_perfil/perfil_componentes.dart';
 //Importa os widgets do Flutter e o componente PerfilComponentes criado na pasta Widgets
 
@@ -11,34 +13,55 @@ class Perfil extends StatefulWidget {
 class _PerfilState extends State<Perfil> {
   int selectedPerfil = 1;
   bool notificacoes1 = true;
-  bool temaEscuro1 = true;
 //Defini as variáveis que controlam o perfil
+
+  File? fotoPerfil;
+
+  Future<void> editarFoto() async {
+    final picker = ImagePicker();
+    final imagem = await picker.pickImage(source: ImageSource.gallery);
+    if (imagem != null) {
+      setState(() {
+        fotoPerfil = File(imagem.path);
+      });
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
-  //Esse método monta a interface visual da tela
+    //Esse método monta a interface visual da tela
 
     return Scaffold(
       backgroundColor: Colors.black,
-      appBar: AppBar(title: Text('Perfis'), backgroundColor: Colors.grey[850]),
-    //Estrutura a base da tela
+
+      appBar: AppBar(
+        backgroundColor: Colors.grey[850],
+        title: Text('Perfis'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);  // Volta para a tela anterior
+          },
+        ),
+      ),
 
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-      //Espaçamento interno
-
+            //Espaçamento interno
             PerfilComponentes(
               nome: 'Nome Sobrenome',
               nascimento: 'dd/mm/aaaa',
               notificacoes: notificacoes1,
-              temaEscuro: temaEscuro1,
               onNotificacoesChanged: (val) => setState(() => notificacoes1 = val),
-              onTemaChanged: (val) => setState(() => temaEscuro1 = val),
               isSelected: selectedPerfil == 1,
+
+              fotoPerfil: fotoPerfil,
+              onEditarFoto: editarFoto,
             ),
-            //Cria o primeiro card de perfil
+
           ],
         ),
       ),

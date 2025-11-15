5a48107bfc2cec385efed927c7f4623460c1e55b
Victor Johann <victorjohann1404@gmail.com>
2025-11-15 18:47:39 -0300

    Ajustes mínimos em appointmentModel

--- backend/src/models/appointmentModel.js
+++ backend/src/models/appointmentModel.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 
 const ConsultaSchema = new mongoose.Schema({
-	userId: {type: mongoose.Schema.Types.ObjectId, refPath: "User"},
+	userId: { type: mongoose.Schema.Types.ObjectId, refPath: "User", required: true },
 	nomeConsulta: { type: String, required: true },
-	nomeProfissional: { type: String},
+	nomeProfissional: { type: String },
 	endereco: { type: String },
 	horario: { type: String },
 	data: { type: Date, required: true }

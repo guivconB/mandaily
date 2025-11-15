96d453ae4effd6492ee6e37cb82305f160baafa5
Victor Johann <victorjohann1404@gmail.com>
2025-11-15 19:36:30 -0300

    Adição do Enum em tipo no medicationModel

--- backend/src/models/medicationModel.js
+++ backend/src/models/medicationModel.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose';
 const MedicationSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref:"User", required: true },
   nome: { type: String, required: true },
-  tipo: { type: String, required: true }, // comprimido, cápsula, etc.
-  dose: { type: Number, required: true },
-  horarioInicio: { type: String, required: true, match: [/^\d{2}:\d{2}$/, "Formato inválido. Use HH:MM (ex: 08:00)"] }, // Ex: '08:00'
+  tipo: { type: String, enum ["Comprimido", "Líquido", "Pomada", "Gota", "Injeção"] required: true }, // comprimido, cápsula, etc.
+  dose: { type: Number},
+  horarioInicio: { type: String, required: true, match: [/^\d{2}:\d{2}$/, "Formato inválido. Use HH:MM (ex: 08:00)"] },
   dataInicio: { type: Date, required: true },
   dias: { type: String, required: true }, // diário, semanal, etc.
   frequencia: { type: String, required: true }, // Ex: '6 horas'
